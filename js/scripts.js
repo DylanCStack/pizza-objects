e667441ccc562854230a4132c16992f03161e9d1
Dylan Stackhouse <dylan@>
2017-01-31 12:53:32 -0800

    change display to show duplicate output, rather than duplicate inputs to represent multiple-pizza orders

--- js/scripts.js
+++ js/scripts.js
@@ -8,107 +8,15 @@ var sizeDict = {
   "large":18,
   "extra large":22
 };
-function addPizza(id){
-  var cheese, pepperoni, hawaiian, veggie, meat;
-  var radioNames = Math.random().toString();
-  switch (id) {
-    case "addCheese":
-      cheese="checked"
-      break;
-    case "addHawaiian":
-      cheese="checked"
-      hawaiian="checked"
-      break;
-    case "addPepperoni":
-      cheese="checked"
-      pepperoni="checked"
-      break;
-    case "addVegetarian":
-      cheese="checked"
-      veggie="checked"
-      break;
-    case "addMeatlovers":
-      cheese="checked"
-      meat="checked"
-      break;
+function clearPizza(){
+  $("input[type=checkbox]:checked").each(function(){
+    $(this).prop('checked', false);
 
-    default:
-      break;
-
-  }
-  $(".pizzas").append(
-        '<div class="pizza">'+
-          '<label for="size">'+
-            '<input type="radio" name="'+radioNames+'" value="small">'+
-            'Small'+
-          '</label>'+
-          '<label for="size">'+
-            '<input type="radio" name="'+radioNames+'" value="medium" checked>'+
-            'Medium'+
-          '</label>'+
-          '<label for="size">'+
-            '<input type="radio" name="'+radioNames+'" value="large">'+
-            'Large'+
-          '</label>'+
-          '<label for="size">'+
-            '<input type="radio" name="'+radioNames+'" value="extra large">'+
-            'Extra Large'+
-          '</label>'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="cheese" '+ cheese + '>'+
-              'Cheese'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="pepperoni" '+ pepperoni +'>'+
-              'Pepperoni'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="cheese" '+ hawaiian +'>'+
-              'Canadian Bacon'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="pineapple" '+ hawaiian +'>'+
-              'Pineapple'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="sausage" '+ meat +'>'+
-              'Sausage'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="chicken" '+ meat +'>'+
-              'Chicken'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="bacon" '+ meat +'>'+
-              'bacon'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="artichoke" '+ veggie +'>'+
-              'Artichoke'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="onions" '+ veggie +'>'+
-              'Onions'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="green onions" '+ veggie +'>'+
-              'Green Onions'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="tomatoes" '+ veggie +'>'+
-              'Tomatoes'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="sun-dried" '+ veggie +'>'+
-              'Sun-Dried Tomatoes'+
-          '</label>'+
-          '<label for="topping">'+
-            '<input type="checkbox" name="topping"'+ 'value="anchovies" >'+
-              'Anchovies'+
-          '</label>'+
-
-        '</div>');
+  })
+}
+function addPizza(pizza){
+  pizzaPrice = pizza.calculatePrice()
+  return pizzaPrice
 }
 
 function Pizza(size){
@@ -133,21 +41,24 @@ Pizza.prototype = {
 
 }
 
-
+var orderPrice = 0;//maybe change to not be global variable
 $(function(){
   $("div.btn").click(function(){
     var thisID = $(this).attr("id")
-    addPizza(thisID)
+    clearPizza();
   })
 
   $("form#order").submit(function(event){
     event.preventDefault();
-    var orderPrice = 0
+    clearPizza();
 
+    var pizzaPrice = 0;
+    var size;
+    // var pizzaToppings = [];
     $(".pizza").each(function(){
-      var size = $(this).children("label").children("input[type=radio]:checked").val();
+      size = $(this).children("label").children("input[type=radio]:checked").val();
       var newPizza = new Pizza(size)
-
+      pizzaPrice = addPizza(newPizza);
       console.log("should be size:  " + size)
 
       $(this).children("label").children("input[type=checkbox]:checked").each(function(){
@@ -155,12 +66,13 @@ $(function(){
 
         var newTopping = new topping(topper);
         newPizza.toppings.push(newTopping);
+        // pizzaToppings.push(newTopping.name);
       })
 
-      orderPrice += newPizza.calculatePrice();
+      orderPrice += pizzaPrice;
     })
 
-
+    $("#totalOrder").prepend('<p>One '+size+' pizza</p>' + /*'<p>With '+pizzaToppings+'</p>'+*/'<p>Price ' +pizzaPrice+'</p>')
     $("#priceOutput").text("$"+orderPrice);
   })
 })
